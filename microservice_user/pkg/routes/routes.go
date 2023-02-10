c8ad32236d03962880b78eefb54c0c48f32d1e5b
ViniciusGR797 <loritoamarelo@outlook.com>
2023-02-10 00:13:16 -0300

    correções em routes

--- microservice_user/pkg/routes/routes.go
+++ microservice_user/pkg/routes/routes.go
@@ -11,38 +11,38 @@ import (
 func ConfigRoutes(router *gin.Engine, service service.UserServiceInterface) *gin.Engine {
 	main := router.Group("api")
 	{
-		produtos := main.Group("/v1")
+		users := main.Group("/v1")
 		{
 			// Rota que retorna lista de users (GET que dispara método GetUsers controller)
-			produtos.GET("/users", func(c *gin.Context) {
+			users.GET("/users", func(c *gin.Context) {
 				controller.GetUsers(c, service)
 			})
 			// Rota que retorna user pelo ID (GET que dispara método GetUserByID controller)
-			produtos.GET("/users/id/:user_id", func(c *gin.Context) {
+			users.GET("/users/id/:user_id", func(c *gin.Context) {
 				controller.GetUserByID(c, service)
 			})
 			// Rota que retorna users pelo nome (GET que dispara método GetUserByName controller)
-			produtos.GET("/users/name/:user_name", func(c *gin.Context) {
+			users.GET("/users/name/:user_name", func(c *gin.Context) {
 				controller.GetUserByName(c, service)
 			})
 			// Rota que retorna lista de users submissos do seus grupos (GET que dispara método GetSubmissiveUsers controller)
-			produtos.GET("/users/submissives/:user_id", func(c *gin.Context) {
+			users.GET("/users/submissives/:user_id", func(c *gin.Context) {
 				controller.GetSubmissiveUsers(c, service)
 			})
-			// Rota que cadastra usuários (POST que dispara método CreateUser controller)
-			produtos.POST("/users", func(c *gin.Context) {
+			// Rota que cadastra user (POST que dispara método CreateUser controller)
+			users.POST("/users", func(c *gin.Context) {
 				controller.CreateUser(c, service)
 			})
-			// Rota que altera status (PUT que dispara método UpdateStatusUser controller)
-			produtos.PUT("/users/update/status/:user_id", func(c *gin.Context) {
+			// Rota que altera status ativo/inativo (PUT que dispara método UpdateStatusUser controller)
+			users.PUT("/users/update/status/:user_id", func(c *gin.Context) {
 				controller.UpdateStatusUser(c, service)
 			})
-			// Rota que retorna usuário editado (PUT que dispara método Update controller)
-			produtos.PUT("/users/update/:user_id", func(c *gin.Context) {
+			// Rota que edita user (PUT que dispara método UpdateUser controller)
+			users.PUT("/users/update/:user_id", func(c *gin.Context) {
 				controller.UpdateUser(c, service)
 			})
-			// Rota que retorna usuário editado (PUT que dispara método Update controller)
-			produtos.POST("/users/login", func(c *gin.Context) {
+			// Rota de login de user (POST que dispara método Login controller)
+			users.POST("/users/login", func(c *gin.Context) {
 				controller.Login(c, service)
 			})
 		}
