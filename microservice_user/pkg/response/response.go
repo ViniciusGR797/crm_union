3eac4fe20251aeabb44db5709e06f4ec78798104
erich-ika <erich.sabe.tudo@gmail.com>
2023-02-20 12:57:46 -0300

    Erros seguem um padrão
    exemplo:
    {
      "error": "incorrect credentials",
      "path": "/union/v1/users/login",
      "status": 401,
      "statusText": "Unauthorized"
    }

---
+++ microservice_user/pkg/response/response.go
@@ -0,0 +1,26 @@
+package response
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+// Error organiza um JSON para responder ao client em caso de erro
+func Error(c *gin.Context, status int, err error) {
+	c.JSON(status, gin.H{
+		"status":     status,
+		"statusText": http.StatusText(status),
+		"error":      err.Error(),
+		"path":       c.FullPath(),
+	})
+}
+
+// Send envia o objeto ao client em caso de sucesso na requisição
+func Send(c *gin.Context, code int, obj any) {
+	c.JSON(code, obj)
+}
+
+func SendNoContent(c *gin.Context) {
+	c.Status(http.StatusNoContent)
+}
