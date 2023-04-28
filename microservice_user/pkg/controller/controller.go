a28569ec14436c533e9c61d01671859d24378631
leohlima <leolimalp@hotmail.com>
2023-04-28 15:18:03 -0300

    fix users connections

--- microservice_user/pkg/controller/controller.go
+++ microservice_user/pkg/controller/controller.go
@@ -192,8 +192,10 @@ func CreateUser(c *gin.Context, service service.UserServiceInterface) {
 		return
 	}
 
+	ctx := c.Request.Context()
+
 	// Chama método Create passando user como parâmetro, cadastra no banco user
-	_, err = service.CreateUser(user, &logID)
+	_, err = service.CreateUser(user, &logID, ctx)
 	// Verifica se teve erro na criação de user
 	if err != nil {
 		sendError(c, http.StatusInternalServerError, err)
@@ -238,8 +240,10 @@ func UpdateStatusUser(c *gin.Context, service service.UserServiceInterface) {
 		return
 	}
 
+	ctx := c.Request.Context()
+
 	// Chama método UpdateStatusUser passando id como parâmetro
-	result, err := service.UpdateStatusUser(&newID, &logID)
+	result, err := service.UpdateStatusUser(&newID, &logID, ctx)
 	if err != nil {
 		sendError(c, http.StatusInternalServerError, err)
 		return
@@ -306,8 +310,10 @@ func UpdateUser(c *gin.Context, service service.UserServiceInterface) {
 		return
 	}
 
+	ctx := c.Request.Context()
+
 	// Chama método UpdateUser passando user e id como parâmetro
-	idResult, err := service.UpdateUser(&newId, user, &logID)
+	idResult, err := service.UpdateUser(&newId, user, &logID, ctx)
 	// Verifica se teve erro na edição de user
 	if err != nil {
 		sendError(c, http.StatusInternalServerError, err)
