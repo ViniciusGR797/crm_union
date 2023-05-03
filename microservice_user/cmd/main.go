f6ae81fdf8dc74f64419bdbb956571ad87764756
ViniciusGR797 <loritoamarelo@outlook.com>
2023-05-03 12:03:11 -0300

    correções user

--- microservice_user/cmd/main.go
+++ microservice_user/cmd/main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"encoding/json"
-	"io"
+	"io/ioutil"
 	"log"
 	"os"
 
@@ -10,7 +10,6 @@ import (
 	"microservice_user/config"
 	"microservice_user/pkg/database"
 	"microservice_user/pkg/routes"
-	"microservice_user/pkg/security"
 	"microservice_user/pkg/server"
 	"microservice_user/pkg/service"
 )
@@ -21,20 +20,20 @@ func main() {
 	default_conf := &config.Config{}
 
 	// Abre o arquivo JSON com as variáveis de ambiente
-	file, err := os.Open("microservice_user/env.json") // file.json has the json content
+	file, err := os.Open("./env.json") // file.json has the json content
 	if err != nil {
-		panic(err)
+		log.Print(err)
 	}
 
 	// Lé todo JSON e transforma em um JSON byte
-	jsonByte, err := io.ReadAll(file)
+	jsonByte, err := ioutil.ReadAll(file)
 	if err != nil {
-		panic(err)
+		log.Print(err)
 	}
 
 	// Converte JSON byte em uma struct, no caso a struct default_conf
 	if err := json.Unmarshal(jsonByte, &default_conf); err != nil {
-		panic(err)
+		log.Print(err)
 	}
 
 	// Atribui para conf as novas configurações do sistema
@@ -50,17 +49,12 @@ func main() {
 	// Cria serviços de um user (CRUD) com a pool de conexão passada por parâmetro
 	service := service.NewUserService(dbpool)
 
-	// Configura a chave de segurança dos tokens
-	err = security.SecretConfig(conf)
-	if err != nil {
-		panic(err)
-	}
-
 	// Cria servidor HTTP com as config passadas por parâmetro
 	serv := server.NewServer(conf)
 
 	// Cria rotas passsando o servidor HTTP e os serviços do user (CRUD)
 	router := routes.ConfigRoutes(serv.SERVER, service)
+
 	// Se tiver ativada a interface de usuário, criar as rotas para o front end (WEB UI)
 	// if conf.WEB_UI {
 	// 	webui.RegisterUIHandlers(router)
